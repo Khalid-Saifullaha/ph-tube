2a02c9f6ffb27180d59b78f1a202910a4cacaa47
Your Name <khalidsaifullaha3@gmail.com>
2024-10-02 19:19:05 +0600

    navbar done

---
+++ script js/video.js
@@ -0,0 +1,24 @@
+// 1. fetch, Load and Show Categories on html
+
+// create loadCatagories
+
+const loadCatagories = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(res => res.json())
+    .then(data => displayCategories(data.categories))
+    .catch(error => console.log(error))
+};
+
+// Create DisplayCategories
+const displayCategories = (categories)=> {
+    categories.forEach(item => {
+        console.log(item);
+        // create a button
+
+        const button = document.createElement('button');
+        button.classList = 'btn';
+        button.innerText = item.category;
+    })
+}
+
+loadCatagories()
