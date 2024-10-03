25a190d1b792564fb4917de9d8fcd835cecfb4df
Your Name <khalidsaifullaha3@gmail.com>
2024-10-03 13:18:16 +0600

    added details button

--- script js/video.js
+++ script js/video.js
@@ -7,6 +7,14 @@ function getTime(time){
      remainingSecond= remainingSecond % 60;
     return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
 }
+
+const removeActiveClass = ()=> {
+    const buttons = document.getElementsByClassName('category-btn');
+    console.log(buttons);
+    for(let btn of buttons){
+        btn.classList.remove('active');
+    }
+}
 // 1. fetch, Load and Show Categories on html
 // create loadCatagories
 
@@ -23,29 +31,66 @@ const loadVideos = () => {
     .catch(error => console.log(error))
 };
 
-// const cardDemo = {
-//     "category_id": "1001",
-//     "video_id": "aaaa",
-//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-//     "title": "Shape of You",
-//     "authors": [
-//         {
-//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-//             "profile_name": "Olivia Mitchell",
-//             "verified": ""
-//         }
-//     ],
-//     "others": {
-//         "views": "100K",
-//         "posted_date": "16278"
-//     },
-//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// }
+const loadCatagoriesVideos = (id) => {
+    // alert(id);
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(res => res.json())
+    .then(data => {
+        // sobaike active class remove koro
+        removeActiveClass();
+
+        // id er class k active koro
+        const activeBtn = document.getElementById(`btn-${id}`);
+        activeBtn.classList.add('active')
+        displayVideos(data.category)
+    })
+    .catch(error => console.log(error))
+};
+
+const loadDetails = async (videoId) => {
+    console.log(videoId);
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayDetails(data.video);
+}
+const displayDetails = (video)=> {
+console.log(video);
+const detailContainer = document.getElementById('modal-content');
+
+detailContainer.innerHTML= `
+<img src=${video.thumbnail}/>
+<p>${video.description}</p>
+`;
+
+// way-1
+document.getElementById('showModalData').click();
+// way-2
+// document.getElementById('customModal').showModal();
+};
 
 const displayVideos = (videos) =>{
     const videoContainer = document.getElementById('videos');
+    videoContainer.innerHTML ="";
+
+   if(videos.length == 0){
+    videoContainer.classList.remove('grid')
+    videoContainer.innerHTML = `
+    <div class = "min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+    <img src="assets/Icon.png" />
+    <h2 class = "text-center text-xl font-bold">
+    No Contend Here in this Category
+    </h2>
+    </div>
+    `;
+    return;
+   }
+   else{
+    videoContainer.classList.add('grid')
+   }
+
     videos.forEach(video => {
-        console.log(video);
+        // console.log(video);
         const card = document.createElement('div');
         card.classList = "card card-compact";
         card.innerHTML = 
@@ -73,7 +118,7 @@ const displayVideos = (videos) =>{
             video.authors[0].verified == true ? `<img class="w-5 " src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>` : ''
         }
        </div>
-    <p></p>
+    <p><button onclick="loadDetails('${video.video_id}')" class = "btn btn-sm  btn-error">Details</button></p>
     </div>
   </div>
         `;
@@ -88,12 +133,15 @@ const displayCategories = (categories)=> {
         console.log(item);
         // create a button
 
-        const button = document.createElement('button');
-        button.classList = 'btn';
-        button.innerText = item.category;
-
+        const buttonContainer = document.createElement('div');
+        buttonContainer.innerHTML = 
+        `
+        <button id = "btn-${item.category_id}" onclick = "loadCatagoriesVideos(${item.category_id})" class = "btn category-btn">
+        ${item.category}
+        </button>
+        `
         // add button to category container
-        categoryContainer.append(button)
+        categoryContainer.append(buttonContainer)
     })
 }
 
--- ts.js
+++ ts.js
@@ -4,11 +4,11 @@
 // console.log(getTime(4320));
 
 function getTime(time){
+    // get hour and rest second
     const hour = parseInt(time / 3600);
     let remainingSecond = time % 3600;
     const minute = parseInt(remainingSecond / 60);
      remainingSecond= remainingSecond % 60;
-    
     return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
 }
 console.log(getTime(7865));
